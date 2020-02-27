fcdad04a28b42bdae80268bca83935787e8bb697
scott7rj <scott7rj@hotmail.com>
2020-02-27 10:40:19 -0300

    exception.py

---
+++ exception.py
@@ -0,0 +1,26 @@
+import logging
+import time
+
+#create logger
+logging.basicConfig(filename = "E:\\python_lessons\\problems.log", level = logging.DEBUG)
+logger = logging.getLogger()
+
+def read_file_timed(path):
+    """return the contents of the file at 'path' and measure time required ."""
+    start_time = time.time()
+    try:
+        f = open(path, mode="rb")]
+        data = f.read()
+        return data
+    except FileNotFoundError as err:
+        logger.error(err)
+        raise
+    else:
+        f.close()
+    finally:
+        stop_time = time.time()
+        dt = stop_time - start_time
+        logger.info("time required for {file} = {time}".format(file=path, time=dt))
+
+data_read_file("E:\\python_lessons\\audio_file.wav")
+    
