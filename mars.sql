e75188fe4afae2bdc08009fa23429ddc93f8689d
scott7rj <scott7rj@hotmail.com>
2020-02-27 11:34:25 -0300

    mars.sql

---
+++ mars.sql
@@ -0,0 +1,16 @@
+martian TABLE
+
+martian_id
+first_name
+last_name
+base_id
+super_id
+
+insert into martian (martian_id, first_name, last_name, base_id, super_id) VALUES ()
+
+base TABLE
+base_id
+base_name
+founded
+
+insert into base (base_id, base_name, founded) VALUES ()
